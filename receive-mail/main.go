37d8452679ad3ba70c2cfeefaaaf7d52eade1a7a
lucas-10101 <lucas-quadros@outlook.com>
2025-09-25 12:33:07 -0300

    lambda stable v1

--- receive-mail/main.go
+++ receive-mail/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
 	"compress/gzip"
 	"context"
@@ -11,10 +12,12 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/sqs"
 )
 
@@ -26,8 +29,8 @@ type MailRequest struct {
 	Bcc     []string `json:"bcc"`
 }
 
-func validateMailRequest(mr *MailRequest) error {
-	if mr.Subject == "" || mr.Body == "" || len(mr.To) == 0 || len(mr.Cc) == 0 || len(mr.Bcc) == 0 {
+func validateMailRequest(mail *MailRequest) error {
+	if mail.Subject == "" || mail.Body == "" || len(mail.To) == 0 || len(mail.Cc) == 0 || len(mail.Bcc) == 0 {
 		return errors.New("all MailRequest fields must be filled")
 	}
 	return nil
@@ -35,75 +38,92 @@ func validateMailRequest(mr *MailRequest) error {
 
 func handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 
-	var body []byte
 	var err error
-	var payload MailRequest
+	var payload *MailRequest
 
-	body, err = getBody(request)
+	payload, err = getPayload(request)
 	if err != nil {
 		return events.APIGatewayProxyResponse{
 			StatusCode: http.StatusInternalServerError,
-			Body:       http.StatusText(http.StatusInternalServerError),
+			Body:       err.Error(),
 		}, nil
 	}
 
-	err = json.Unmarshal(body, &payload)
-	if err != nil {
-		return events.APIGatewayProxyResponse{
-			StatusCode: http.StatusInternalServerError,
-			Body:       http.StatusText(http.StatusInternalServerError),
-		}, nil
-	}
-	body = nil
-
-	if err = validatePayload(&payload); err != nil {
+	if err = validateMailRequest(payload); err != nil {
 		return events.APIGatewayProxyResponse{
 			StatusCode: http.StatusBadRequest,
 			Body:       err.Error(),
 		}, nil
 	}
 
-	sqsClient := sqs.NewFromConfig(aws.Config{})
-	queueURL := os.Getenv("MAIL_QUEUE_URL")
-
-	_, err = sqsClient.SendMessage(context.Background(), &sqs.SendMessageInput{
-		QueueUrl:    aws.String(queueURL),
-		MessageBody: aws.String(string(body)),
-	})
+	config, err := config.LoadDefaultConfig(ctx)
 	if err != nil {
 		return events.APIGatewayProxyResponse{
 			StatusCode: http.StatusInternalServerError,
-			Body:       http.StatusText(http.StatusInternalServerError),
+			Body:       err.Error(),
+		}, nil
+	}
+
+	sqsClient := sqs.NewFromConfig(config)
+	queueURL := os.Getenv("MAIL_QUEUE_URL")
+
+	messageId, err := enqueueMailMessage(ctx, sqsClient, queueURL, payload)
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			StatusCode: http.StatusInternalServerError,
+			Body:       err.Error(),
 		}, nil
 	}
 
 	return events.APIGatewayProxyResponse{
-		StatusCode: 200,
-		Body:       "Mail received and validated",
+		StatusCode: http.StatusOK,
+		Body:       "Message enqueued with ID: " + *messageId,
 	}, nil
+}
+
+func enqueueMailMessage(ctx context.Context, sqsClient *sqs.Client, queueURL string, mailRequest *MailRequest) (*string, error) {
+
+	jsonData, err := json.Marshal(mailRequest)
+	if err != nil {
+		return nil, err
+	}
+
+	base64Data := base64.StdEncoding.EncodeToString(jsonData)
+	jsonData = nil
+
+	out, err := sqsClient.SendMessage(ctx, &sqs.SendMessageInput{
+		QueueUrl:    aws.String(queueURL),
+		MessageBody: aws.String(base64Data),
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return out.MessageId, nil
 }
 
 func getPayload(request events.APIGatewayProxyRequest) (*MailRequest, error) {
 
-}
+	var body []byte
+	var err error
+	var payload MailRequest
 
-func validatePayload(payload *MailRequest) error {
-	if payload == nil {
-		return errors.New(http.StatusText(http.StatusBadRequest))
+	body, err = getBodyBytes(request)
+	if err != nil {
+		return nil, err
 	}
 
-	if payload.Subject == "" {
-		return errors.New("subject is required")
-	} else if payload.Body == "" {
-		return errors.New("body is required")
-	} else if len(payload.To) == 0 && len(payload.Cc) == 0 && len(payload.Bcc) == 0 {
-		return errors.New("at least one recipient (to, cc, bcc) is required")
+	err = json.Unmarshal(body, &payload)
+	if err != nil {
+		return nil, err
 	}
+	body = nil
 
-	return nil
+	return &payload, nil
 }
 
-func getBody(request events.APIGatewayProxyRequest) ([]byte, error) {
+func getBodyBytes(request events.APIGatewayProxyRequest) ([]byte, error) {
 
 	var data []byte
 	var err error
@@ -136,11 +156,33 @@ func getBody(request events.APIGatewayProxyRequest) ([]byte, error) {
 	return data, err
 }
 
+func readEnvFile() {
+	file, err := os.Open(".env")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		lineSplit := strings.SplitAfterN(line, "=", 2)
+		key := strings.TrimSpace(lineSplit[0])
+		value := strings.TrimSpace(lineSplit[1])
+
+		if err := os.Setenv(key, value); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
+
 func main() {
 
-	if os.Getenv("FROM_EMAIL") == "" {
-		log.Fatal("FROM_EMAIL environment variable is not set")
-	}
+	readEnvFile()
 
 	lambda.Start(handler)
 }
